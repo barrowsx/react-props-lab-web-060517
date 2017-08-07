0517e35c7da127bd6b8597597e2f7bba0238faa6
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-08-07 16:48:26 -0400

    𝔠𝔬𝔪𝔪𝔦𝔱𝔱𝔢𝔡 𝔲𝔰𝔦𝔫𝔤 𝔞𝔯𝔠𝔞𝔫𝔢 𝔪𝔞𝔤𝔦𝔠𝔨𝔰 𝔩𝔬𝔰𝔱 𝔱𝔬 𝔱𝔥𝔦𝔰 𝔪𝔬𝔯𝔱𝔞𝔩 𝔠𝔬𝔦𝔩

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,1 +1,25 @@
 // Code The Spaceship Component Here
+import React from 'react';
+
+class Spaceship extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+        <ul>
+          <li>Speed: {this.props.speed}</li>
+          <li>Has Rockets?: {this.props.hasRockets}</li>
+          <li>Colors: {this.props.colors}</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+Spaceship.defaultProps = {
+  speed: 'slow',
+  hasRockets: false,
+  colors: ['black', 'red']
+}
+
+export default Spaceship
